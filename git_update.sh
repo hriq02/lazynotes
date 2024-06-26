4aaf7a2ad16888d3c13e91b0463e27e1dc81029c
hriq02 <150181760+hriq02@users.noreply.github.com>
2024-06-26 14:58:02 -0300

    Delete git_update.sh

--- git_update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")" # Change to the directory where the script is located
-read -p "Enter your commit message: " commit_message
-git add .
-git commit -m "$commit_message"
-git push origin HEAD
